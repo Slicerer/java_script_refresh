07f56cf02fb0d14d76f425808e23b9a3abd0a0f5
Tipouroux <perfectum@outlook.fr>
2023-08-11 13:24:37 -0400

    try again ca vas rentrer

--- exercice_2/factor.js
+++ exercice_2/factor.js
@@ -1,1 +1,1 @@
-//bon je push ca dans le main a fn de pratiquer se commentaire sera detruit sur la branche divergente et l'Exercisse sera ajouter et merger dans le main ici meme
+//bon je push ca dans la branch fifferé a fn de pratiquer se commentaire sera detruit sur la branche divergente et l'Exercisse sera ajouter et merger dans le main ici meme
