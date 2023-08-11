58454e015609cfcb055a2bb0c6fc2d9676241aa8
Tipouroux <perfectum@outlook.fr>
2023-08-11 12:55:28 -0400

    try to push only on different branch

---
+++ exercice_1/covert.js
@@ -0,0 +1,25 @@
+/*
+	Écrivez une fonction qui prend en entrée une température en degrés Celsius et la convertit en degrés Fahrenheit.
+
+	°F = (°C × 9/5) + 32
+*/
+
+const fromCelToFaraneith = (celcius) => celcius * 9 / 5 + 32;
+
+
+
+console.log(fromCelToFaraneith(20));
+
+
+//ajouter quelque chose sur la branch degre_to_faraneith a des fait de pratiqe de merge
+
+const scratch = (celcius) => celcius * 9 / 5 + 32;
+
+
+
+console.log(fromCelToFaraneith(20));
+const proh = (celcius) => celcius * 9 / 5 + 32;
+
+
+
+console.log(fromCelToFaraneith(20));
