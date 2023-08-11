bb4f081fef51e023b5ac56079cee540f5f1c7302
Tipouroux <perfectum@outlook.fr>
2023-08-11 13:54:05 -0400

    first merge have been done

--- exercice_1/covert.js
+++ exercice_1/covert.js
@@ -6,20 +6,7 @@
 
 const fromCelToFaraneith = (celcius) => celcius * 9 / 5 + 32;
 
-
-
 console.log(fromCelToFaraneith(20));
-
-
-//ajouter quelque chose sur la branch degre_to_faraneith a des fait de pratiqe de merge
 
 const scratch = (celcius) => celcius * 9 / 5 + 32;
 
-
-
-console.log(fromCelToFaraneith(20));
-const proh = (celcius) => celcius * 9 / 5 + 32;
-
-
-
-console.log(fromCelToFaraneith(20));
